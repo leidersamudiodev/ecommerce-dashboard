5c09354c6e253b2ceb845f12b94a04e139f35c93
Leider Samudio <leider@debian.debian.com>
2026-04-16 11:47:58 -0500

    Actualizar Detalles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/dash.svg',
+        url: '/icon-light-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/dash.svg',
+        url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
